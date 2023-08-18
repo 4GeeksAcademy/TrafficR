088f8af2bb7720ab1fa6b38174e3158d596231f8
Rick Nuñez <113080329+RickNunez@users.noreply.github.com>
2023-08-18 21:32:49 +0000

    Semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+const [verde,setVerde] = useState("secondary")
+const [rojo,setRojo] = useState("secondary")
+const [amarillo,setAmarillo] = useState("secondary")
+
+
+
+function handleTrafficLightVerde(){
+ setRojo("secondary")
+ setAmarillo("secondary")
+ setVerde("success")
+}
+function handleTrafficLightRed(){
+  setRojo("danger")
+  setAmarillo("secondary")
+  setVerde("secondary")
+ }
+ function handleTrafficLightAmarillo(){
+  setRojo("secondary")
+  setAmarillo("warning")
+  setVerde("secondary")
+ }
+ function handleNewColor() {
+  if (rojo === "danger") {
+    handleTrafficLightAmarillo();
+  } else if (amarillo === "warning") {
+    handleTrafficLightVerde();
+  } else {
+    handleTrafficLightRed();
+  }
+}
+
+
+  return (
+    <> 
+  
+    <div className="container-fluid">
+      <div className="semaforo justify-content-center" style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "150px", border: "1px solid gray", background: "gray"}} >
+   
+  <button className= {"btn btn-" + rojo + " p-5" + " rounded-circle" } onClick={handleTrafficLightRed}>
+  
+    </button>
+  <button className= {"btn btn-" + amarillo + " p-5" + " rounded-circle"} onClick={handleTrafficLightAmarillo} >
+  
+    </button>
+    <button className= {"btn btn-" + verde + " p-5" + " rounded-circle"} onClick={handleTrafficLightVerde}>
+		
+    </button>
+
+      </div>
+    </div> 
+    <div className="alternar ">
+    <button
+  className={"btn btn-primary m-5"}
+  onClick={handleNewColor}>Alternar
+</button>
+      </div> 
+   </> 
+  );
 };
 
 export default Home;
